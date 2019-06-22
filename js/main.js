9016a965cc22444cec6128ef3c3377d78236a98c
ar78ilnik <arakchiev.ilya@yandex.ru>
2019-06-23 00:50:50 +0300

    module3-task1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// 1. Создание массива объявлений
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var WIDTH_LOCATION = 1200;
 var HEIGHT_LOCATION = 750;
@@ -15,43 +14,39 @@ var getRandomNumber = function (maxNumber) {
 };
 
 var createPinObjects = function (pinsCount) {
-  var pins = [];
+  var Arraypins = [];
   for (var i = 0; i < pinsCount; i++) {
     var pin = {
       author: {avatar: 'img/avatars/user0' + (i + 1) + '.png'},
       offer: {type: getRandomValue(TYPES)},
       location: {x: (getRandomNumber(WIDTH_LOCATION) + 25), y: getRandomNumber(HEIGHT_LOCATION)}
     };
-    pins.push(pin);
+    Arraypins.push(pin);
   }
-  return pins;
+  return Arraypins;
 };
 
 var pins = createPinObjects(8);
 
-// 2. Убираем класс .map--faded
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
-
-// 3. Создание DOM-элементов
 
 var pinsTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapPins = document.querySelector('.map__pins');
 
-var renderPin = function () {
+var renderPin = function (pinValues) {
   var pinElement = pinsTemplate.cloneNode(true);
-  pinElement.style = 'left: ' + pins[i].location.x + 'px; top: ' + pins[i].location.y + 'px;';
-  pinElement.src = pins[i].author.avatar;
-  pinElement.alt = pins[i].offer.type;
-  console.log(pinElement);
+  pinElement.style = 'left: ' + pinValues.location.x + 'px; top: ' + pinValues.location.y + 'px;';
+  pinElement.firstChild.src = pinValues.author.avatar;
+  pinElement.firstChild.alt = pinValues.offer.type;
   return pinElement;
 };
 
-// 4. Отрисовка сгенерированных элементов в блок .map__pins
 var fragment = document.createDocumentFragment();
 
 for (var i = 0; i < pins.length; i++) {
-  fragment.appendChild(renderPin());
+  var pin = pins[i];
+  fragment.appendChild(renderPin(pin));
 }
 
 mapPins.appendChild(fragment);
