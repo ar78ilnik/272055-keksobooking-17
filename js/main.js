26bf360e3fee66282893c7aed6745e75f782f1e6
ar78ilnik <arakchiev.ilya@yandex.ru>
2019-06-27 00:13:08 +0300

    05

--- js/main.js
+++ js/main.js
@@ -4,58 +4,27 @@ var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var WIDTH_LOCATION = 1200;
 var HEIGHT_LOCATION = 750;
 
-var pinsTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var form = document.querySelector('.ad-form');
-
 var fields = form.getElementsByTagName('fieldset');
 for (var i = 0; i < fields.length; i++) {
   fields[i].setAttribute('disabled', 'disabled');
 }
 
-var mapPins = document.querySelector('.map__pins');
 var mapFilters = document.querySelector('.map__filters');
 mapFilters.setAttribute('disabled', 'disabled');
 
-var pinMain = document.querySelector('.map__pin--main');
-pinMain.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
+var mapPins = document.querySelector('.map__pins');
+mapPins.setAttribute('disabled', 'disabled');
 
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-  
-  var onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-    dragged = true;
+var mapPin = document.querySelector('.map__pin--main');
+mapPin.setAttribute('disabled', 'disabled');
 
-    var shift = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
-
-
+mapPin.addEventListener('click', function () {
+  for (i = 0; i < fields.length; i++) {
+    fields[i].removeAttribute('disabled');
   }
-  var onMouseUp = function (moveEvt) {
-    moveEvt.preventDefault();
-
-    var shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
-    };
-
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
-
-    pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
-    pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
-  };
-
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-});
+  mapPins.removeAttribute('disabled');
+})
 
 var getRandomValue = function (values) {
   var index = Math.floor(Math.random() * values.length);
@@ -66,9 +35,11 @@ var getRandomNumber = function (maxNumber) {
   return Math.floor(Math.random() * maxNumber);
 };
 
+var pinsTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
 var createPinObjects = function (pinsCount) {
   var Arraypins = [];
-  for (var i = 0; i < pinsCount; i++) {
+  for (i = 0; i < pinsCount; i++) {
     var pin = {
       author: {avatar: 'img/avatars/user0' + (i + 1) + '.png'},
       offer: {type: getRandomValue(TYPES)},
@@ -97,3 +68,7 @@ for (var i = 0; i < pins.length; i++) {
 }
 
 mapPins.appendChild(fragment);
+
+var mapPins = document.querySelector('.map__pins');
+mapPins.setAttribute('disabled', 'disabled');
+
